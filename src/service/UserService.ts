f24fb7630058d4980696f4c859b550e3f4149d98
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-04-08 19:27:31 -0400

    add user service

---
+++ src/service/UserService.ts
@@ -0,0 +1,11 @@
+import { User } from '../domain/User';
+import { UserRepository } from '../repository/UserRepository';
+
+export class UserService {
+    constructor(private userRepository: UserRepository) {}
+
+    async getUser(id: number): Promise<User> {
+        const userDto = await this.userRepository.getUser(id);
+        return new User(userDto.id, userDto.name, userDto.email);
+    }
+}
