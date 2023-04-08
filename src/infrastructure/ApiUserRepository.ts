a51819e0b282b61df44861c147d69b9612fc75e6
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2023-04-08 19:33:45 -0400

    add infra user api

---
+++ src/infrastructure/ApiUserRepository.ts
@@ -0,0 +1,10 @@
+import { UserRepository } from '../repository/UserRepository';
+
+export class ApiUserRepository implements UserRepository {
+    constructor(private apiUrl: string) {}
+
+    async getUser(id: number): Promise<any> {
+        const response = await fetch(`${this.apiUrl}/users/${id}`);
+        return response.json();
+    }
+}
